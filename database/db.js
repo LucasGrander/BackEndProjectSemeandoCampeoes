e56e6b94f8f03d55ed3fbb32f4319757680e3e01
RICARDO BATISTA <batista.4995@aluno.pr.senac.br>
2024-06-10 09:43:27 -0300

    backend1006

--- database/db.js
+++ database/db.js
@@ -1,13 +1,23 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+// export const db = mysql.createConnection({
+//     host:"viaduct.proxy.rlwy.net",
+//     port: "45380",
+//     user: "root",
+//     password:"ldkBdBpMuMxSssiXlgJcgyVmvDknTzVb",
+//     database: "railway"
+// })
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-    host:"viaduct.proxy.rlwy.net",
-    port: "45380",
-    user: "root",
-    password:"ldkBdBpMuMxSssiXlgJcgyVmvDknTzVb",
-    database: "railway"
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 })
-
 db.connect((err) => {
     if(err){
         console.log(`Erro ao tentar se conectar com o banco de dados`)
